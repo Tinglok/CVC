c441993313daf47cd6d5519bf7ae5ad161ba2bd8
Tingle Li <tinglok.lai@gmail.com>
2020-11-02 22:54:35 +0800

    Update compute_similarity.py

--- compute_similarity.py
+++ compute_similarity.py
@@ -12,21 +12,20 @@ encoder = VoiceEncoder()
 
 cvc_list, cyclegan_list = [], []
 for cvc_path, cyclegan_path in zip(os.listdir(cvc_dir), os.listdir(cyclegan_dir)):
+    cvc_path = os.path.join(cvc_dir, cvc_path)
+    cyclegan_path = os.path.join(cyclegan_dir, cyclegan_path)
 
-	cvc_path = os.path.join(cvc_dir, cvc_path)
-	cyclegan_path = os.path.join(cyclegan_dir, cyclegan_path)
+    cvc_wav, _ = read_wav(cvc_path, sr=sampling_rate)
+    cyclegan_wav, _ = read_wav(cyclegan_path, sr=sampling_rate)
 
-	cvc_wav, _ = read_wav(cvc_path, sr=sampling_rate)
-	cyclegan_wav, _ = read_wav(cyclegan_path, sr=sampling_rate)
-
-	cvc_list.append(cvc_wav)
-	cyclegan_list.append(cyclegan_wav)
+    cvc_list.append(cvc_wav)
+    cyclegan_list.append(cyclegan_wav)
 
 target_list = []
 for target_path in os.listdir(target_dir):
-	target_path = os.path.join(target_dir, target_path)
-	target_wav, _ = read_wav(target_path, sr=sampling_rate)
-	target_list.append(target_wav)
+    target_path = os.path.join(target_dir, target_path)
+    target_wav, _ = read_wav(target_path, sr=sampling_rate)
+    target_list.append(target_wav)
 
 spk_embeds_cvc = np.array([encoder.embed_speaker(cvc_list)])
 spk_embeds_cyclegan = np.array([encoder.embed_speaker(cyclegan_list)])
